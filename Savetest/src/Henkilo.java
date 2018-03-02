c1d3b73752274b5dca1198048c4e90285cead0d5
teemu <teemu@DESKTOP-UOQP61S>
2018-03-02 15:41:27 +0200

    Henkilon ja Talon metodit

--- Savetest/src/Henkilo.java
+++ Savetest/src/Henkilo.java
@@ -1,4 +1,20 @@
 
 public class Henkilo {
-
+	
+	private String nimi;
+	private int ika;
+	
+	public Henkilo(String nimi, int ika) {
+		this.nimi = nimi;
+		this.ika = ika;
+	}
+	
+	public String annaNimi() {
+		return nimi;
+	}
+	
+	public int annaIka() {
+		return ika;
+	}
+	
 }
--- Savetest/src/Talo.java
+++ Savetest/src/Talo.java
@@ -1,4 +1,25 @@
+import java.util.ArrayList;
 
 public class Talo {
 
+	private int pintaAla;
+	private ArrayList<Henkilo> asukkaat;
+	
+	public Talo(int pintaAla, ArrayList<Henkilo> asukkaat) {
+		this.pintaAla = pintaAla;
+		this.asukkaat = new ArrayList<>();
+		for(Henkilo h : asukkaat) {
+			this.asukkaat.add(h);
+		}
+	}
+	
+	public void tulostaTiedot() {
+		System.out.println("Pinta-ala: " + pintaAla);
+		String asuk = "Asukkaat: ";
+		for(Henkilo h : asukkaat) {
+			asuk += h.annaNimi() + ", " + h.annaIka() + "  ";
+		}
+		System.out.println(asuk);
+	}
+	
 }
